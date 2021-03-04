02f43aa54e707701172f237457b609a5ba02ada3
Roman Yarovoi <roman.and.yarovoi@gmail.com>
2021-03-04 22:17:17 +0200

    feat: implement brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0) return false
+
+  pairs = bracketsConfig.map(item => item.join(''))
+
+  let i = 0
+  while (i <= pairs.length) {
+    if (str.includes(pairs[i])) {
+      str = str.replace(pairs[i], '')
+      i = 0 
+    } else { 
+      i++
+    }
+  }
+  return !str.length 
 }
+
